811aead4d2c6ba8dca921643d3d93350dc8d84e7
kunal757 <kunal@laptop96.tresata.com>
2024-07-03 14:19:43 +0530

    third commit

--- .scala-build/Scala_learn_f4dd477a3a/src_generated/main/FrenchDate.scala
+++ .scala-build/Scala_learn_f4dd477a3a/src_generated/main/FrenchDate.scala
@@ -9,10 +9,13 @@ import java.time.LocalDate
 import java.util.Locale._
 
 object FrenchDate {
+  val t1=System.nanoTime()
   def main(args: Array[String]): Unit = {
     val now = LocalDate.now
     val df = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(FRANCE)
     println(df.format(now))
+    val duration=(System.nanoTime()-t1)/1e9d
+    println(duration)
   }
 }
 
